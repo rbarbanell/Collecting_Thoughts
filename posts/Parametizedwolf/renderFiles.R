0a1fdbb40b3175b029706b378b708832e1529796
Becca <rebecca.barbanell@gmail.com>
2023-05-25 13:29:47 -0700

    change code

--- posts/Parametizedwolf/renderFiles.R
+++ posts/Parametizedwolf/renderFiles.R
@@ -1,9 +1,9 @@
-library(rmarkdown)
+library(quarto)
 library(purrr)
-Document.Maker <- function(x) {
-  render("Simple-Parameterized-Markdown.qmd", 
-         params = list(name = x), 
+Equity.Analyser <- function(x) {
+  quarto_render("Simple-Parameterized-Markdown.qmd", 
+         execute_params = list(name = x), 
          output_file = paste0(x,".html"))
 }
 Example <- data.frame(name = c("Joe","John","Andrew"))
-Example$name %>% map(., function(x) { Document.Maker(x)})
+Example$name %>% map(., function(x) { Equity.Analyser(x)})
