e2626322cf6a22577f30ada2bbac9b3d8c9d406b
Becca <rebecca.barbanell@gmail.com>
2023-06-05 19:35:54 -0700

    small updates

--- posts/Parametizedwolf/renderFiles.R
+++
@@ -1,9 +0,0 @@
-library(quarto)
-library(purrr)
-Equity.Analyser <- function(x) {
-  quarto_render("Simple-Parameterized-Markdown.qmd", 
-         execute_params = list(name = x), 
-         output_file = paste0(x,".html"))
-}
-Example <- data.frame(name = c("Joe","John","Andrew"))
-Example$name %>% map(., function(x) { Equity.Analyser(x)})
